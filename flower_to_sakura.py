514f5d2a64acd295075428794926583080bb485c
Tacro Hashimoto <tacro@g.ucla.edu>
2018-10-16 16:29:40 +0900

    basically good

--- flower_to_sakura.py
+++ flower_to_sakura.py
@@ -5,25 +5,23 @@ from math import pi
 class MorningGlory:
     def __init__(self):
         # Draw the outer circle
-        fill(127)
+        fill(200)
         circle_center = (random_uniform(low = 300, high = 500), random_uniform(low = 300, high = 500))
         r1 = random_uniform(low=180, high=270)
-        circle(circle_center, 2 * r1)
+        # circle(circle_center, 2 * r1)
         # Draw the inner circle
         fill(220)
         r2 = (sin(0.94) - sqrt(2)*2/5) * r1
         circle(circle_center, 2 * r2)
         # Draw 5 circular sectors by using lines
         fill(0)
-        for i in range(4):
+        for i in range(1, 6):
             start = Vector((circle_center[0] +  r2 * cos(i * 0.4 * pi)), (circle_center[1] + r2 * sin(i * 0.4 * pi)))
-            # start = ((circle_center[0] + r2 * cos(0.2 * pi)), (circle_center[1] + r2 * sin(0.2 * pi)))
-            for angle in range(0, 150, 1):
-                radian = radians(angle/2)
-                end = Vector((circle_center[0] + r1 * cos(radian * i)), (circle_center[1] + r1 * sin(radian * i)))
+            noise = random_uniform(high = 1.2, low = 1)
+            for angle in range(45, 315 , 1):
+                radian =  radians(angle/3 + i*72)
+                end = Vector((start.x + noise * (r1 - r2) * cos(radian)), (start.y + noise * (r1 - r2) * sin(radian)))
                 line(start, end)
-
-
 
 def setup():
     size(800, 800)
