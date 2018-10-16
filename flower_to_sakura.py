04e49409049254db8b7a3083e948e9c3f4c4351d
Tacro Hashimoto <tacro@g.ucla.edu>
2018-10-16 19:13:18 +0900

    now MorningGlory class works fine :)

--- flower_to_sakura.py
+++ flower_to_sakura.py
@@ -5,27 +5,26 @@ from math import pi
 class MorningGlory:
     def __init__(self):
         # Draw the outer circle
-        fill(200)
-        circle_center = (random_uniform(low = 300, high = 500), random_uniform(low = 300, high = 500))
-        r1 = random_uniform(low=180, high=270)
-        # circle(circle_center, 2 * r1)
+        center = Vector(random_uniform(low = 300, high = 500), random_uniform(low = 300, high = 500))
+        r1 = random_uniform(low=200, high=250)
         # Draw the inner circle
-        fill(220)
+        fill(220,217,207)
         r2 = (sin(0.94) - sqrt(2)*2/5) * r1
-        circle(circle_center, 2 * r2)
+        circle(center, 2 * r2)
         # Draw 5 circular sectors by using lines
         fill(0)
+        rad54 = radians(54)
         for i in range(1, 6):
-            start = Vector((circle_center[0] +  r2 * cos(i * 0.4 * pi)), (circle_center[1] + r2 * sin(i * 0.4 * pi)))
-            noise = random_uniform(high = 1.2, low = 1)
-            for angle in range(45, 315 , 1):
+            start = Vector((center.x +  r2 * cos(i * 0.4 * pi + rad54)), (center.y + r2 * sin(i * 0.4 * pi + rad54)))
+            for angle in range(0, 300, 1):
+                noise = random_uniform(high = 1.02, low = 1)
                 radian =  radians(angle/3 + i*72)
                 end = Vector((start.x + noise * (r1 - r2) * cos(radian)), (start.y + noise * (r1 - r2) * sin(radian)))
                 line(start, end)
 
 def setup():
     size(800, 800)
-    background(255)
+    background(120)
 
 # def draw():
     # if key == 'ENTER':
